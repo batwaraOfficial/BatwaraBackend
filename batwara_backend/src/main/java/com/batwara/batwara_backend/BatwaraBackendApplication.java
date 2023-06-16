7acfeba8d06797e82fd8a646b16fd1e3a2d57625
Sahil Mahajan <smahajan54842@gmail.com>
2023-06-16 19:41:37 +0530

    Backend_Setup

---
+++ batwara_backend/src/main/java/com/batwara/batwara_backend/BatwaraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.batwara.batwara_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatwaraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatwaraBackendApplication.class, args);
+	}
+
+}
---
+++ batwara_backend/src/test/java/com/batwara/batwara_backend/BatwaraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batwara.batwara_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatwaraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
